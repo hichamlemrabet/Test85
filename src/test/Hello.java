f12e8b97eade68113d46676b22174b74a5e0bfdb
Hicham Lemrabet <hicham.lemrabet@student.ehb.be>
2020-04-14 14:25:03 +0200

    Test2

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hicham Lemrabet");
         System.out.println("Haytham Arihi");
+        System.out.println("Hicham Lemrabet2");
     }
 }
